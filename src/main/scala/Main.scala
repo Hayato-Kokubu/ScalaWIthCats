8bfd5ea1a45e585d7710a6277327b3a47116e050
hayatokokubu <kokubu.hayato@gmail.com>
2019-07-11 13:32:27 +0900

    CH1 Summary

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,8 @@
-import java.time.LocalDateTime
-
-import cats.syntax.eq._
-import EqImplicits._
-
 object Main extends App{
 
-  val x = LocalDateTime.of(2019, 7, 11, 13, 10, 0)
-  val y = LocalDateTime.of(2019, 7, 11, 13, 10, 1)
-
-  val b1 = x.equals(y)
-  val b2 = x === y
-
-  println(b1)  // 普通に比較するとfalse
-  println(b2)  // EqImplicits の比較方法ではtrue
 
 }
+
+sealed trait A
+final case object B extends A
+final case object C extends A
