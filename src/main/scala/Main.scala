7b7ee143f4a11bd9b8d12b2d14bf119552f9d2dc
hayatokokubu <kokubu.hayato@gmail.com>
2019-07-11 10:40:15 +0900

    use === , =!=

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,12 @@
 import cats.Eq
 import cats.instances.int._  // Eq[Int]で Eq[Int]が必要
-
+import cats.syntax.eq._  // to use === or =!=
 
 object Main extends App{
-  val eqInt = Eq[Int]
 
+  val t1 = 123 === 123
+  val t2 = 123 =!= 234
 
-  val t1 = eqInt.eqv(123, 123)
-  val t2 = eqInt.eqv(123, 234)
-//  val t3 = eqInt.eqv(123, "234") // compile error
 
   println(t1)
   println(t2)
