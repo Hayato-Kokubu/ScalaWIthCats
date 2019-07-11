699b9e4aa56ba51edb5e9c19527e00a52aee806a
hayatokokubu <kokubu.hayato@gmail.com>
2019-07-11 18:01:59 +0900

    fix problem1.3

--- src/main/scala/printable/Printable.scala
+++ src/main/scala/printable/Printable.scala
@@ -7,18 +7,18 @@ trait Printable[A] {
 object Printable {
   def format[A](value: A)(implicit printable: Printable[A]): String = format(value)
 
-  def print[A](value: A)(implicit printable: Printable[A]): Unit = println(value)
+  def print[A](value: A)(implicit printable: Printable[A]): Unit = println(format(value))
 }
 
 
 object PrintableInstances{
 
   implicit object StringPrintable extends Printable[String] {
-    override def format(value: String): String = ???
+    override def format(value: String): String = value
   }
 
   implicit object IntPrintable extends Printable[Int] {
-    override def format(value: Int): String = ???
+    override def format(value: Int): String = value.toString
   }
 }
 
