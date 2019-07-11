4a880a6c6b78071f29d5bd1b63f1416fe1fde5d5
hayatokokubu <kokubu.hayato@gmail.com>
2019-07-11 10:35:08 +0900

    example of Eq

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,36 +1,17 @@
-import java.time.LocalDate
+import cats.Eq
+import cats.instances.int._  // Eq[Int]で Eq[Int]が必要
 
-import cats.Show
-import cats.instances.int._
-import cats.instances.string._
-import ShowImplisits._
 
 object Main extends App{
-
-  val showInt = Show.apply[Int]       // implicitly[Show[Int]] と同じ
-  val showString = Show.apply[String]
-  val showDate = Show.apply[LocalDate]
-  val showBox = Show.show[Box](b => s"box has ${b.value}.")
-
-  val showBottle = Show.fromToString[Bottle]
+  val eqInt = Eq[Int]
 
 
-  val intAsString: String = showInt.show(123)
-  val stringAsString: String = showString.show("abc")
-  val dateAsString: String = showDate.show(LocalDate.of(2019, 7, 10))
-  val boxAsString: String = showBox.show(Box("apple")) // implicit value is difined companion object  show method
-  val bottleAsString: String = showBottle.show(new Bottle("water"))
+  val t1 = eqInt.eqv(123, 123)
+  val t2 = eqInt.eqv(123, 234)
+//  val t3 = eqInt.eqv(123, "234") // compile error
 
-  println(s"intAsString = $intAsString")
-  println(s"stringAsString = $stringAsString")
-  println(s"dateAsString = $dateAsString")
-  println(s"boxAsString = $boxAsString")
-  println(s"bottleAsString = $bottleAsString")
+  println(t1)
+  println(t2)
 
-}
 
-case class Box(value: String)
-
-class Bottle(value: String){
-  override def toString: String = s"$value in the bottle"
 }
