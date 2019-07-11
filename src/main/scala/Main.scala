a4f66e00ce762faf1d0915295266f1f7f95ca637
hayatokokubu <kokubu.hayato@gmail.com>
2019-07-11 12:47:00 +0900

    Optionの復習

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,28 @@
 import cats.Eq
 import cats.instances.int._  // Eq[Int]で Eq[Int]が必要
 import cats.syntax.eq._  // to use === or =!=
+import cats.instances.option._
 
 object Main extends App{
 
-  val t1 = 123 === 123
-  val t2 = 123 =!= 234
+//  val t1 = Some(1) === None // None がOption[Int]だと見なされない
+  val t2 = (Some(1) : Option[Int]) === (None : Option[Int])
+  val t3 = Option(1) === Option.empty[Int] // left side: not Some , right side: empty method で型を入れられる
+
+  // Optionの復習
+  val o1 = Option(null) // null はNullのinstance なので、型はOption[Null]
+  val o2 = Option(1) // Some[Int]
+
+  val nullStr: String  = null // String のインスタンスにnull を格納
+  val o3 = Option(nullStr) //
+  val o4 = Option(None)    // Option[None.type]
+  val o5 = Option(Some(1)) // Some はclass なので、Option[Some] となる
+  val o6 = Option() // Option[Unit]
 
 
-  println(t1)
-  println(t2)
+  println(o1)
+  println(o2)
+  println(o3)
 
 
 }
