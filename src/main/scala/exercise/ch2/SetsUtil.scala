666222cb3c1d949342743b16c1bfe994592277e1
hayatokokubu <kokubu.hayato@gmail.com>
2019-07-12 21:18:40 +0900

    solve 2.4 refactor

--- src/main/scala/exercise/ch2/SetsUtil.scala
+++ src/main/scala/exercise/ch2/SetsUtil.scala
@@ -2,9 +2,9 @@ package exercise.ch2
 
 object SetsUtil {
 
-  implicit val setsMonoid = new Monoid[Set[String]]{
-    override def combine(x: Set[String], y: Set[String]): Set[String] = x ++ y
-    val empty = Set.empty[String]
+  implicit def setsMonoid[T] = new Monoid[Set[T]]{
+    override def combine(x: Set[T], y: Set[T]): Set[T] = x ++ y
+    val empty = Set.empty[T]
   }
 
 }
