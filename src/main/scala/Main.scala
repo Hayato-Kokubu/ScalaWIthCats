23c89533631f57ff961c6ca236bb8f3d72ec0ef1
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-07-13 10:27:59 +0900

    use cats.Monoid

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,11 @@
-import exercise.ch2.Monoid
-import exercise.ch2.SetsUtil._
+import cats.kernel.Monoid
+import cats.instances.string._
 
 object Main extends App{
 
+  val s1 = Monoid[String].combine("hoge", "fuga")
+  val s2 = Monoid[String].empty
 
-  val s1 = Set("hello", "world")
-  val s2 = Set("hoge", "fuga")
-
-  val sm = implicitly[Monoid[Set[String]]]
-
-  val s3 = sm.combine(s1, s2)
-
-  println(s3)
-
-
-
+  println(s1)
+  println(s2)
 }
