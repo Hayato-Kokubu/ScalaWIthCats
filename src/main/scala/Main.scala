23c05c109c28b0e92e32f0be5df900498f31b278
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-07-13 11:32:58 +0900

    use cats.syntax.semigroup._  |+|

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,11 @@
-import cats.Monoid
 import cats.instances.string._
-import cats.instances.int._
-import cats.instances.option._
-import cats.syntax.option._
+import cats.syntax.semigroup._
 
 object Main extends App{
-  val m1 = Monoid[String].combine("hoge", "fuga")
+  //Monoid[String].combine("hoge", "fuga")
+  val m1 =  "hoge" |+| "fuga"
+
   println(m1)
 
-  val m2 = Monoid[Int].combine(20, 30)
-  println(m2)
-
-  val m3 = Monoid[Option[Int]].combine(3.some , 10.some)
 
 }
