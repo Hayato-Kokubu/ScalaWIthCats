fcf230a5490fc5c0e2a4fe4e71993fa625bf0f9b
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-07-13 22:54:36 +0900

    solve 2.5.4-2 myself

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,16 @@
 import exercise.ch2.superAdder.SuperAdder
+import cats.instances.int._
+import cats.instances.option._
+
+import cats.syntax.option._
 
 object Main extends App{
 
 
-  val result = new SuperAdder().add(List(1,2,3))
+  val resultInt = new SuperAdder[Int].add(List(1,2,3))
+  val resultOptionInt = new SuperAdder[Option[Int]].add(List(1.some,2.some,3.some, none))
 
-  println(result)
-
+  println(resultInt)
+  println(resultOptionInt)
 
 }
--- src/main/scala/exercise/ch2/superAdder/SuperAdder.scala
+++ src/main/scala/exercise/ch2/superAdder/SuperAdder.scala
@@ -1,13 +1,11 @@
 package exercise.ch2.superAdder
 
 import cats.Monoid
-
-import cats.instances.int._
 import cats.syntax.semigroup._
 
-class SuperAdder {
-  def add(items: List[Int]): Int =
-    items.foldLeft(0){case (res, acc) =>
+class SuperAdder[A] {
+  def add(items: List[A])(implicit m: Monoid[A]): A =
+    items.foldLeft( m.empty ){case (res, acc) =>
       res |+| acc
   }
 }
