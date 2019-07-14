f6cf6fc40e20c6a3b69907b1206a5081bed5d651
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-07-14 14:08:17 +0900

    3.4 type constructor , higher kinds

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,25 +1,9 @@
-import cats.instances.function._
-import cats.syntax.functor._
+import cats.Functor
 
 object Main extends App{
 
-  val func1: Int => Double =
-    (x: Int) => x.toDouble
-
-  val func2: Double => Double =
-    (y: Double) => y * 2
-
-  // map が解決されないが、動く
-  val res7 = (func1 map func2)(1)     // composition using map
-  // res7: Double = 2.0
-  println(s"res7 = $res7")
-
-  val res8 = (func1 andThen func2)(1) // composition using andThen
-  // res8: Double = 2.0
-  println(s"res8 = $res8")
-
-  val res9 = func2(func1(1))          // composition written out by hand
-  // res9: Double = 2.0
-  println(s"res9 = $res9")
+  def myMethod[F[_]] = {
+    val functor = Functor.apply[F]
+  }
 
 }
