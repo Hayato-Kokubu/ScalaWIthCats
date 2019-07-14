9f9bd9e666639d241286f4ec31eb205871e194be
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-07-14 20:11:04 +0900

    3.5.2 exaple of map1 : andThen

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,18 @@
-import cats.Functor
-
-import cats.instances.option._
-
-import cats.syntax.option._
+import cats.instances.function._
+import cats.syntax.functor._
 
 object Main extends App{
 
-  val func: Int => Int = x => x + 1
+  val func1 = (a: Int) => a + 1
+  val func2 = (a: Int) => a * 2
+  val func3 = (a: Int) => a + "!"
 
-  val liftedFunc = Functor[Option].lift(func)
+  val func4 = func1.map(func2).map(func3)
 
-  val r1 = liftedFunc(3.some)
-  val r2 = liftedFunc(none)
+  val func5 = func1.andThen(func2).andThen(func3)
 
-  println(r1)
-  println(r2)
-
+  val res1 = func4(10)
+  val res2 = func5(10)
+  println(res1)
+  println(res2)
 }
