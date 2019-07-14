319da20c05eda9b880c49be1a5700da117d172cf
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-07-14 16:30:16 +0900

    Functer.lift

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,24 +1,19 @@
 import cats.Functor
 
-import cats.instances.list._
 import cats.instances.option._
+
+import cats.syntax.option._
 
 object Main extends App{
 
-  val list1 = List(1, 2, 3)
-  // list1: List[Int] = List(1, 2, 3)
+  val func: Int => Int = x => x + 1
 
-  val list2 = Functor[List].map(list1)(_ * 2)
-  // need cats.Functor, cats.instances.list._
-  // list2: List[Int] = List(2, 4, 6)
-  println(list2)
+  val liftedFunc = Functor[Option].lift(func)
 
-  val option1 = Option(123)
-  // option1: Option[Int] = Some(123)
+  val r1 = liftedFunc(3.some)
+  val r2 = liftedFunc(none)
 
-  val option2 = Functor[Option].map(option1)(_.toString + "!")
-  // option2: Option[String] = Some(123)
-  // need cats.instances.option._
-  println(option2)
+  println(r1)
+  println(r2)
 
 }
