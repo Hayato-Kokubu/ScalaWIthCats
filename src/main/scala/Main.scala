9693633567a9b40e1c914a8e5aa6055e4e80a93a
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-07-15 15:50:51 +0900

    Ch4 start

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,40 +1,41 @@
-import cats.Functor
-import exercise.ch3.{Branch, Leaf, Tree}
-import exercise.ch3.TreeImplicits._
-
 object Main extends App {
 
-  val tree = Tree.branch(
-    Tree.branch(
-      Tree.leaf(1),
-      Tree.branch(
-        Tree.leaf(2),
-        Tree.branch(Tree.leaf(3), Tree.branch(Tree.leaf(4), Tree.leaf(5)))
-      ),
-    ),
-    Tree.branch(
-      Tree.branch(
-        Tree.leaf(6),
-        Tree.branch(
-          Tree.leaf(7),
-          Tree.leaf(8)
-        )
-      ),
-      Tree.branch(
-        Tree.leaf(9),
-        Tree.leaf(10)
-      )
-    ),
-  )
+  def parseInt(str: String): Option[Int] =
+    scala.util.Try(str.toInt).toOption
 
-//  implicitly[Functor[Tree]]
-//  println(tree)
+  def divide(a: Int, b: Int): Option[Int] =
+    if(b == 0) None else Some(a / b)
 
-  val fTree = Functor[Tree].map(tree)(i => (i * 10) + "!")
-  println(fTree)
+//  def stringDivideBy(aStr: String, bStr: String): Option[Int] =
+//    parseInt(aStr).flatMap { aNum =>
+//      parseInt(bStr).flatMap { bNum =>
+//        divide(aNum, bNum)
+//      }
+//    }
+
+    def stringDivideBy(aStr: String, bStr: String): Option[Int] =
+      for {
+        aNum <- parseInt(aStr)
+        bNum <- parseInt(bStr)
+        ans  <- divide(aNum, bNum)
+      } yield ans
 
 
+  val res1 = stringDivideBy("6", "2")
+  // res1: Option[Int] = Some(3)
 
-//  tree.map(_ * 2)
+  val res2 = stringDivideBy("6", "0")
+  // res2: Option[Int] = None
+
+  val res3 = stringDivideBy("6", "foo")
+  // res3: Option[Int] = None
+
+  val res4 = stringDivideBy("bar", "2")
+  // res4: Option[Int] = None
+
+  println(res1)
+  println(res2)
+  println(res3)
+  println(res4)
 
 }
