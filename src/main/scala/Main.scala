32604875af3c206874ba4edb69e54fc75e81d718
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-07-16 22:00:26 +0900

    4.1.1 monad type class

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,41 +1,30 @@
-object Main extends App {
+import cats.Monad
+import cats.instances.option._ // for Monad
+import cats.instances.list._   // for Monad
 
-  def parseInt(str: String): Option[Int] =
-    scala.util.Try(str.toInt).toOption
+object Main extends App{
+  val opt1 = Monad[Option].pure(3)
+  // opt1: Option[Int] = Some(3)
+  println(opt1)
 
-  def divide(a: Int, b: Int): Option[Int] =
-    if(b == 0) None else Some(a / b)
+  val opt2 = Monad[Option].flatMap(opt1)(a => Some(a + 2))
+  // opt2: Option[Int] = Some(5)
+  println(opt2)
 
-//  def stringDivideBy(aStr: String, bStr: String): Option[Int] =
-//    parseInt(aStr).flatMap { aNum =>
-//      parseInt(bStr).flatMap { bNum =>
-//        divide(aNum, bNum)
-//      }
-//    }
+  val opt3 = Monad[Option].map(opt2)(a => 100 * a)
+  // opt3: Option[Int] = Some(500)
+  println(opt3)
 
-    def stringDivideBy(aStr: String, bStr: String): Option[Int] =
-      for {
-        aNum <- parseInt(aStr)
-        bNum <- parseInt(bStr)
-        ans  <- divide(aNum, bNum)
-      } yield ans
+  val list1 = Monad[List].pure(3)
+  // list1: List[Int] = List(3)
+  println(list1)
 
+  val list2 = Monad[List].
+    flatMap(List(1, 2, 3))(a => List(a, a*10))
+  // list2: List[Int] = List(1, 10, 2, 20, 3, 30)
+  println(list2)
 
-  val res1 = stringDivideBy("6", "2")
-  // res1: Option[Int] = Some(3)
-
-  val res2 = stringDivideBy("6", "0")
-  // res2: Option[Int] = None
-
-  val res3 = stringDivideBy("6", "foo")
-  // res3: Option[Int] = None
-
-  val res4 = stringDivideBy("bar", "2")
-  // res4: Option[Int] = None
-
-  println(res1)
-  println(res2)
-  println(res3)
-  println(res4)
-
+  val list3 = Monad[List].map(list2)(a => a + 123)
+  // list3: List[Int] = List(124, 133, 125, 143, 126, 153)
+  println(list3)
 }
