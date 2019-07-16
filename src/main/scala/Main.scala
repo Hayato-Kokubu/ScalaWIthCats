fb832e72ea0b495340d37452d72ae4d8401d389c
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-07-16 22:09:57 +0900

    4.1.2  Default Instances

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,30 +1,40 @@
 import cats.Monad
-import cats.instances.option._ // for Monad
-import cats.instances.list._   // for Monad
+import cats.instances.option._
+
+import scala.concurrent.Future // for Monad
 
 object Main extends App{
-  val opt1 = Monad[Option].pure(3)
-  // opt1: Option[Int] = Some(3)
-  println(opt1)
+  val opt = Monad[Option].flatMap(Option(1))(a => Option(a*2))
+  // opt: Option[Int] = Some(2)
+  println(opt)
 
-  val opt2 = Monad[Option].flatMap(opt1)(a => Some(a + 2))
-  // opt2: Option[Int] = Some(5)
-  println(opt2)
 
-  val opt3 = Monad[Option].map(opt2)(a => 100 * a)
-  // opt3: Option[Int] = Some(500)
-  println(opt3)
 
-  val list1 = Monad[List].pure(3)
-  // list1: List[Int] = List(3)
-  println(list1)
+  import cats.instances.list._ // for Monad
 
-  val list2 = Monad[List].
-    flatMap(List(1, 2, 3))(a => List(a, a*10))
-  // list2: List[Int] = List(1, 10, 2, 20, 3, 30)
-  println(list2)
+  val list = Monad[List].flatMap(List(1, 2, 3))(a => List(a, a*10))
+  // list: List[Int] = List(1, 10, 2, 20, 3, 30)
+  println(list)
 
-  val list3 = Monad[List].map(list2)(a => a + 123)
-  // list3: List[Int] = List(124, 133, 125, 143, 126, 153)
-  println(list3)
+  import cats.instances.vector._ // for Monad
+
+
+  val vec = Monad[Vector].flatMap(Vector(1, 2, 3))(a => Vector(a, a*10))
+  // vec: Vector[Int] = Vector(1, 10, 2, 20, 3, 30)
+  println(vec)
+
+
+  import cats.instances.future._  // <- catsStdInstancesForFuture
+  import scala.concurrent.ExecutionContext.Implicits.global // <- EcecutuionContext
+  import scala.concurrent._
+  import scala.concurrent.duration._
+
+
+  val fm = Monad[Future]
+
+  val future = fm.flatMap(fm.pure(1))(x => fm.pure(x + 2))
+
+  val res = Await.result(future, 1.second)
+  println(res)
+
 }
