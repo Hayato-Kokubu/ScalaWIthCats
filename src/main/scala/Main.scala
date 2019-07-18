6d6d03e9815e6817fbbd78d75e812ca028501ee8
hayatokokubu <kokubu.hayato@gmail.com>
2019-07-18 10:24:04 +0900

    4.3.1 solve exercise myself

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,47 +1,13 @@
-import scala.language.higherKinds
-import cats.Monad
-import cats.syntax.functor._
-import cats.syntax.flatMap._
-import cats.syntax.applicative._
-import cats.instances.option._
-import cats.instances.list._
-import cats.instances.future._
-import scala.concurrent.ExecutionContext.Implicits.global
-
-import scala.concurrent.Future
-
+import cats.Id
+import exercise.ch4.MyId
 
 object Main extends App {
-  def sumSquare[F[_]: Monad](a: F[Int], b: F[Int]): F[Int] =
-    for {
-      x <- a
-      y <- b
-    } yield x*x + y*y
 
-//  val optx = 2.pure[Option]
-//  val opty = 3.pure[Option]
-//
-//  val optRes = sumSquare[Option](optx, opty)
-//  println(optRes)
+  val a = MyId.pure(1)
 
-//  val listx = 2.pure[List]
-//  val listy = 3.pure[List]
-//
-//  val listRes = sumSquare(listx, listy)
-//  println(listRes)
+  println(a)
 
+  val s = MyId.map(a)(_ * 10.0d)
 
-//  val futx = 2.pure[Future]
-//  val futy = 3.pure[Future]
-//
-//  val futRes = sumSquare(futx, futy)
-//  println(futRes)
-//  Thread.sleep(100L)
-//  println(futRes)
-
-
-    // no type parameter と怒られる。。。
-//    val idRes = sumSquare(2, 3)
-//    println(idRes)
-
+  println(s)
 }
