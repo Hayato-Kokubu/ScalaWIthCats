fe95f36095b94d2ee3dbfdbf9625fa3b73672164
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-07-21 22:50:17 +0900

    Eval.defer トランポリンの意味があまりよくわかってない

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,16 @@
 import cats.Eval
 
 object Main extends App {
-  val ans = for {
-    a <- Eval.later { println("Calculating A"); 40 }
-    b <- Eval.always { println("Calculating B"); 2 }
-  } yield {
-    println("Adding A and B")
-    a + b
-  }
 
 
-  println("----------")
-  ans.value
-  println("----------")
-  ans.value
-  println("----------")
+  def factorial(n: BigInt): Eval[BigInt] =
+    if(n == 1) {
+      Eval.now(n)
+    } else{
+      Eval.defer {factorial(n - 1).map(_ * n) }
+    }
 
+  val res = factorial(1000000).value
+
+  println(res)
 }
