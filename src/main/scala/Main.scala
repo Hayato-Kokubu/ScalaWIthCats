efc4cbcedbdb1a6a534e4a140c74b3d44dbffb52
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-07-21 21:16:44 +0900

    4.6.3 eval as a monad

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,24 +1,14 @@
 import cats.Eval
 
 object Main extends App {
+  val greeting = Eval.
+    always { println("Step 1"); "Hello" }.
+    map { str => println("Step 2"); s"$str world" }
 
-  lazy val now = Eval.now({println("now!"); math.random + 1000})
-
-  lazy val later = Eval.later({println("later!"); math.random + 2000})
-
-  lazy val always = Eval.always({println("always!"); math.random + 3000})
-
-
-  println("-------------")
+  println("--------------")
+  println(greeting.value)
+  println("--------------")
+  println(greeting.value)
 
 
-  println(now.value)
-  println(later.value)
-  println(always.value)
-
-  println("-------------")
-
-  println(now.value)
-  println(later.value)
-  println(always.value)
 }
