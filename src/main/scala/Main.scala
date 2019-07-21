073a181e73f49d15ea5964e706e38547b2321764
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-07-21 21:36:53 +0900

    for式ver.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,19 @@
 import cats.Eval
 
 object Main extends App {
-  val greeting = Eval.
-    now { println("Step 1"); "Hello" }.
-    map { str => println("Step 2"); s"$str world" }
+  val ans = for {
+    a <- Eval.later { println("Calculating A"); 40 }
+    b <- Eval.always { println("Calculating B"); 2 }
+  } yield {
+    println("Adding A and B")
+    a + b
+  }
 
-  println("--------------")
-  println(greeting.value)
-  println("--------------")
-  println(greeting.value)
 
+  println("----------")
+  ans.value
+  println("----------")
+  ans.value
+  println("----------")
 
 }
