133ab36dcaf2649340bbb64ab6eff4e27b70a5d5
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-07-28 22:43:06 +0900

    generally two arbitary monads can't be composed

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,24 @@
-import cats.Eval
+import cats.Monad
+import cats.syntax.applicative._ // for pure
+import cats.syntax.flatMap._     // for flatMap
+import scala.language.higherKinds
 
+
+// Hypothetical example. This won't actually compile:
 object Main extends App {
-  def foldRight[A, B](as: List[A], acc: B)(fn: (A, B) => B): Eval[B] =
-    as match {
-      case head :: tail =>
-        fn(head, foldRight(tail, acc)(Eval.defer()))
-//        fn(head, foldRight(tail, acc)(fn))
-      case Nil =>
-        Eval.now(acc)
-//        acc
+
+  def compose[M1[_]: Monad, M2[_]: Monad] = {
+    type Composed[A] = M1[M2[A]]
+
+    new Monad[Composed] {
+      def pure[A](a: A): Composed[A] =
+        a.pure[M2].pure[M1]
+
+      def flatMap[A, B](fa: Composed[A])
+                       (f: A => Composed[B]): Composed[B] =
+      // Problem! How do we write flatMap?
+        ???
     }
+  }
+
 }
