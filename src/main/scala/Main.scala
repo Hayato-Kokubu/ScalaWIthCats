762bff6ec06d7cfb8b0adff783c6f1ef3746053e
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-08-03 23:29:00 +0900

    duplicate for expression with Cats

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,113 +1,34 @@
 import scala.language.higherKinds
-import Implicits._
+import cats.instances.option._
+import cats.instances.future._
+import cats.syntax.applicative._
 
 
-import scala.concurrent.Future
+import scala.concurrent.Future // for pure
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.{Failure, Success, Try}
-
 
 object Main extends App {
 
-  val mOp = implicitly[Monad[Option]]
-  val mFut = implicitly[Monad[Future]]
 
-  val futureOption1 = mFut.pure( mOp.pure(1) )
-  val futureOption2 = mFut.pure( mOp.pure(2) )
+  // use import cats.syntax.applicative._
+  // use import cats.instances.option._
+  val futureOption1 = 2.pure[Option].pure[Future]
+  val futureOption2 = 3.pure[Option].pure[Future]
 
+  val res =
+    for{
+      opt1 <- futureOption1
+      opt2 <- futureOption2
+    } yield {
+      for{
+        n1 <- opt1
+        n2 <- opt2
+      }yield n1 + n2
+    }
 
-
-   // compile error
-//  val resX =
-//    for {
-//      opt1 <- futureOption1
-//      n1 <- opt1
-//      opt2 <- futureOption2
-//      n2 <- opt2
-//    } yield n1 + n2
-
-  // 二重for式つらみ
-  val res = for{
-    opt1 <- futureOption1
-    opt2 <- futureOption2
-    res = for{
-      n1 <- opt1
-      n2 <- opt2
-    } yield n1 + n2
-  } yield res
-
+  Thread.sleep(100)
   println(res)
-
-
-
-  val mEither = implicitly[Monad[MyEither]]
-  val futureEither1 = mFut.pure(mEither.pure(2))
-  val futureEither2 = mFut.pure(mEither.pure(3))
-
-  val resFutureEither = for{
-    e1 <- futureEither1
-    e2 <- futureEither2
-    res = for{
-      n1 <- e1
-      n2 <- e2
-    } yield n1 + n2
-  } yield res
-
-
-  println(resFutureEither)
 }
 
 
 
-// map は、pure と flatMap から作成可能
-trait Monad[F[_]] {
-  def pure[A](a: A): F[A]
-
-  def flatMap[A,B](fa: F[A])(f: A => F[B]): F[B]
-}
-
-object Implicits {
-
-  implicit val optionMonad: Monad[Option] = {
-    new Monad[Option]{
-      def pure[A](a: A): Option[A] = Option(a)
-
-      def flatMap[A,B](fa: Option[A])(f: A => Option[B]): Option[B] = {
-        fa match {
-          case Some(a) => f(a)
-          case None => None
-//          case Option.empty[A] => Option.empty[B]
-        }
-      }
-    }
-  }
-
-  type MyEither[T] = Either[Error,T]
-  implicit val eitherMonad: Monad[MyEither] = {
-    new Monad[MyEither]{
-      def pure[A](a: A): MyEither[A] = Right(a)
-
-      def flatMap[A,B](fa: MyEither[A])(f: A => MyEither[B]): MyEither[B] ={
-        fa match {
-          case Right(a) => f(a)
-          case Left(e)  => Left(e)
-        }
-      }
-    }
-  }
-
-  implicit val futureMonad: Monad[Future] ={
-    new Monad[Future] {
-      def pure[A](a: A): Future[A] = Future(a)
-
-      // Future のflatMap実装そのまま
-      def flatMap[A,B](fa: Future[A])(f: A => Future[B]): Future[B] = {
-        fa.transformWith{
-          case Success(a) => f(a)
-          case Failure(e) => Future.failed[B](e)
-        }
-      }
-    }
-  }
-
-}
