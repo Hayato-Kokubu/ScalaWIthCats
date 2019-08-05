354d0d0fc335cae5d4962748a41852f581f26cb9
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-08-05 21:06:17 +0900

    swap OptionT <=> EitherT

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,29 +1,42 @@
-import cats.data.OptionT
+import cats.data.{EitherT, OptionT}
 
 import scala.language.higherKinds
-//import cats.instances.option._
-import cats.instances.future._
+import cats.instances.either._
 import cats.syntax.applicative._
-
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
+import cats.syntax.either._
 
 object Main extends App {
 
-  // F = Futureであることは、import cats.instances.future._ 内のApplicative で判断
-  // ※ cats.instances.option._ をimport すると、F = Option となる
-  // implicit探索の順番の話 今回は略
-  val futureOpt1 = OptionT.pure(1)
-  val futureOpt2 = OptionT.pure(1)
+  type MyEither[A]  = Either[Error, A]
+  type MyEitherT[A] = EitherT[Option, Error, A]
 
-  val res =
-    for{
-      n1 <- futureOpt1
-      n2 <- futureOpt2
-    } yield n1 + n2
+  val eitherOpt = OptionT.pure[MyEither](1)
 
-  Thread.sleep(100)
-  println(res)
+  println(eitherOpt)
+
+  val optEither = swap2(eitherOpt)
+
+  println(optEither)
+
+
+  def swap1[A](eOpt: MyEitherT[A]): OptionT[MyEither, A] = {
+    import cats.instances.option._
+    eOpt.value  match {
+      case Some(Right(a)) => OptionT[MyEither,A]( a.pure[Option].pure[MyEither])
+      case Some(Left(e))  => OptionT[MyEither,A]( e.asLeft[Option[A]])
+      case None           => OptionT[MyEither,A]( Option.empty[A].pure[MyEither])
+    }
+  }
+
+  def swap2[A](optE: OptionT[MyEither,A]): MyEitherT[A] = {
+    import cats.instances.option._
+    optE.value match {
+      case Right(Some(a)) => EitherT[Option,Error,A](a.asRight[Error].pure[Option])
+      case Right(None)      => EitherT[Option,Error,A](Option.empty[Either[Error,A]])
+      case Left(e)          => EitherT[Option,Error,A](e.asLeft[A].pure[Option])
+    }
+  }
+
 }
 
 
