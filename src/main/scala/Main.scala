f7bfc46e83a91ab98b9618f5963fdb3ad2dcc2ee
hayatokokubu <kokubu.hayato@gmail.com>
2019-08-07 19:15:13 +0900

    wip

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,44 +1,30 @@
-import cats.data.Writer
-import cats.instances.vector._ // for Monoid
-import cats.syntax.applicative._ // for pure
-import cats.syntax.writer._    // for tell apply, writer など
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 object Main extends App {
 
-  type Logged[A] = Writer[Vector[String], A]
+  def slowly[A](body: => A) =
+    try body finally Thread.sleep(100)
 
-  val writer1 = for {
-    a <- 10.pure[Logged]
-    _ <- Vector("a", "b", "c").tell
-    b <- 32.writer(Vector("x", "y", "z"))
-  } yield a + b
-
-  println(writer1)
-
-
-  val writer2 =  writer1.mapWritten(_.map(_.toUpperCase))
-  println(writer2)
-
-  val writer3 = writer1.bimap(
-    log => log.map(_.toUpperCase),
-    res => res * 100
-  )
-
-  println(writer3)
-
-  val writer4 = writer1.mapBoth { (log, res) =>
-    val log2 = log.map(_ + "!")
-    val res2 = res * 1000
-    (log2, res2)
+  def factorial(n: Int): Int = {
+    val ans = slowly(if(n == 0) 1 else n * factorial(n - 1))
+    println(s"fact $n $ans")
+    ans
   }
 
-  println(writer4)
+  val res =
+    Await.result(
+      Future.sequence(
+        Vector(
+          Future(factorial(3)),
+          Future(factorial(3))
+        )
+      ),
+      5.seconds
+    )
 
-  val writer5 = writer1.reset
-  println(writer5)
-
-  val writer6 = writer1.swap
-  println(writer6)
+  println(res)
 
 }
 
