19c278f9dd7fb2363a7f89f14f75cce811179951
hayatokokubu <kokubu.hayato@gmail.com>
2019-08-07 13:01:56 +0900

    Writer method 色々

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,24 +7,39 @@ object Main extends App {
 
   type Logged[A] = Writer[Vector[String], A]
 
-  val x = 123.pure[Logged]
+  val writer1 = for {
+    a <- 10.pure[Logged]
+    _ <- Vector("a", "b", "c").tell
+    b <- 32.writer(Vector("x", "y", "z"))
+  } yield a + b
 
-  val y = Vector("msg1", "msg2", "msg3").tell
+  println(writer1)
 
-  println(x)
-  println(y)
 
-  val a = Writer(Vector("msg1", "msg2", "msg3"), 123)
-  val b = 123.writer(Vector("msg1", "msg2", "msg3"))
+  val writer2 =  writer1.mapWritten(_.map(_.toUpperCase))
+  println(writer2)
 
-  println(a)
-  println(b)
+  val writer3 = writer1.bimap(
+    log => log.map(_.toUpperCase),
+    res => res * 100
+  )
 
-  val aResult: Int = a.value
-  val aLog: Vector[String] = a.written
+  println(writer3)
 
-  println(aResult)
-  println(aLog)
+  val writer4 = writer1.mapBoth { (log, res) =>
+    val log2 = log.map(_ + "!")
+    val res2 = res * 1000
+    (log2, res2)
+  }
+
+  println(writer4)
+
+  val writer5 = writer1.reset
+  println(writer5)
+
+  val writer6 = writer1.swap
+  println(writer6)
+
 }
 
 
