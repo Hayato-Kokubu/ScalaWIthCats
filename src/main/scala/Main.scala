6ad63c4d8a152e23626502a48dc129bca0925552
hayatokokubu <kokubu.hayato@gmail.com>
2019-08-07 12:42:36 +0900

    use Writer

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,48 +1,30 @@
-import cats.syntax.either._ // for asRight, asLeft
+import cats.data.Writer
+import cats.instances.vector._ // for Monoid
+import cats.syntax.applicative._ // for pure
+import cats.syntax.writer._    // for tell apply, writer など
 
 object Main extends App {
 
-  val a = 3.asRight[String]
-  // a: Either[String,Int] = Right(3)
+  type Logged[A] = Writer[Vector[String], A]
 
-  val b = 4.asRight[String]
-  // b: Either[String,Int] = Right(4)
+  val x = 123.pure[Logged]
 
-   val res4 =
-     for {
-       x <- a
-       y <- b
-     } yield x*x + y*y
-  // res4: scala.util.Either[String,Int] = Right(25)
-  println(res4)
+  val y = Vector("msg1", "msg2", "msg3").tell
 
+  println(x)
+  println(y)
 
-//  def countPositive(nums: List[Int]) =
-//    nums.foldLeft(Right(0)) { (accumulator, num) =>
-//      if(num > 0) {
-//        // foldLeft の 初期値がRight であるのに対し、 mapでは Either が返るため エラー
-//        // また、Right.apply では 左側の型指定ができない
-//        accumulator.map(_ + 1)
-//      } else {
-//        Left("Negative. Stopping!")
-//      }
-//    }
+  val a = Writer(Vector("msg1", "msg2", "msg3"), 123)
+  val b = 123.writer(Vector("msg1", "msg2", "msg3"))
 
-  def countPositive(nums: List[Int]) =
-    nums.foldLeft(0.asRight[String]) { (accumulator, num) =>
-      if(num > 0) {
-        accumulator.map(_ + 1)
-      } else {
-        Left("Negative. Stopping!")
-      }
-    }
+  println(a)
+  println(b)
 
-  val res5 = countPositive(List(1, 2, 3, 4))
-  println(res5)
+  val aResult: Int = a.value
+  val aLog: Vector[String] = a.written
 
-  val res6 = countPositive(List(1, -2, 3, 4))
-  println(res6)
-
-  val res7 = "string".asLeft[Int]
-
+  println(aResult)
+  println(aLog)
 }
+
+
