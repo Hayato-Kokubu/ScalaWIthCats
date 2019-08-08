605facb2a46c70b9a99ead76151f9dcdf10aa043
hayatokokubu <kokubu.hayato@gmail.com>
2019-08-08 13:13:24 +0900

    copy solution

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,30 +1,45 @@
+import cats.data.Writer
+import cats.syntax.writer._
+import cats.syntax.applicative._
+import cats.instances.vector._
+
 import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
 object Main extends App {
+  type Logged[A] = Writer[Vector[String], A]
 
   def slowly[A](body: => A) =
     try body finally Thread.sleep(100)
 
-  def factorial(n: Int): Int = {
-    val ans = slowly(if(n == 0) 1 else n * factorial(n - 1))
-    println(s"fact $n $ans")
-    ans
-  }
+  def factorial(n: Int): Logged[Int] =
+    for {
+      ans <- if(n == 0) {
+        1.pure[Logged]
+      } else {
+        slowly(factorial(n - 1).map(_ * n))
+      }
+      _   <- Vector(s"fact $n $ans").tell
+    } yield ans
 
-  val res =
+
+
+  val Vector((logA, ansA), (logB, ansB)) =
     Await.result(
       Future.sequence(
         Vector(
-          Future(factorial(3)),
-          Future(factorial(3))
+          Future(factorial(3).run),
+          Future(factorial(5).run)
         )
       ),
       5.seconds
     )
 
-  println(res)
+  println(logA)
+  println(logB)
+  println(ansA)
+  println(ansB)
 
 }
 
