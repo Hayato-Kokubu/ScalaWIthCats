17a2452613928b885435a396cd900e13823ab4cd
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-08-10 15:21:43 +0900

    copy exercise

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,59 +1,33 @@
 import cats.data.State
-import State._
+import exercise.ch4.PostOrderCalculator
 
 object Main extends App {
-  val step1 = State[Int,String] { num =>
-    val ans = num + 1
-    (ans, s"Result of step1: $ans")
-  }
 
-  val step2 = State[Int, String] { num =>
-    val ans = num * 2
-    (ans, s"Result of step2: $ans")
-  }
+  val a = PostOrderCalculator.evalOne("42").runA(Nil).value
+  println(a)
 
-  val both = for {
-    a <- step1
-    b <- step2
-  } yield (a, b)
+  val program = for {
+    _ <- PostOrderCalculator.evalOne("1")
+    _ <- PostOrderCalculator.evalOne("2")
+    ans <- PostOrderCalculator.evalOne("+")
+  }yield ans
+  val res = program.runA(Nil).value
+  println(res)
 
-  val (state, result) = both.run(20).value
-  println(s"${(state, result)}")
+  val program2 = PostOrderCalculator.evalAll(List("1", "2", "+", "3", "*"))
+  val res2 = program2.run(Nil).value
+  println(res2)
 
-  println("##################")
+  val program3 = for {
+    _   <- PostOrderCalculator.evalAll(List("1", "2", "+"))
+    _   <- PostOrderCalculator.evalAll(List("3", "4", "+"))
+    ans <- PostOrderCalculator.evalOne("*")
+  } yield ans
+  val res3 = program3.run(Nil).value
+  println(res3)
 
-  val getDemo = State.get[Int] // State[Int,Int]
-  val rGet = getDemo.run(10).value
-  println(rGet)
-
-  val setDemo = State.set[Int](30) //State[Int, Unit]
-  val rSet = setDemo.run(10).value
-  println(rSet)
-
-  val pureDemo = State.pure[Int, String]("Result") //State[Int, String]
-  val rPure = pureDemo.run(10).value
-  println(rPure)
-
-  val inspectDemo = State.inspect[Int, String](_ + "!") //State[Int, String]
-  val rInspect = inspectDemo.run(10).value
-  println(rInspect)
-
-  val modifyDemo = State.modify[Int](_ + 1) //State[Int, Unit]
-  val rModify = modifyDemo.run(10).value
-  println(rModify)
+  val program4 = PostOrderCalculator.evalInput("1 2 + 3 4 + *")
+  println(program4)
 
 
-  println("#####################")
-
-
-  val program: State[Int, (Int, Int, Int)] = for {
-    a <- get[Int]
-    _ <- set[Int](a + 1)
-    b <- get[Int]
-    _ <- modify[Int](_ + 1)
-    c <- inspect[Int, Int](_ * 1000)
-  } yield (a, b, c)
-
-  val (stateProgram, resultProgram ) = program.run(1).value
-  println(s"${(stateProgram, resultProgram )}")
 }
