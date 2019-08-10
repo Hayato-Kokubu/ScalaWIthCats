3f78166cf252f474278dd8879f3dfa7422177813
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-08-10 14:43:41 +0900

    State Monad example

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,26 +1,59 @@
 import cats.data.State
+import State._
 
 object Main extends App {
-  val a = State[Int, String] { state =>
-    (state, s"The state is $state")
+  val step1 = State[Int,String] { num =>
+    val ans = num + 1
+    (ans, s"Result of step1: $ans")
   }
 
-  println(a)
+  val step2 = State[Int, String] { num =>
+    val ans = num * 2
+    (ans, s"Result of step2: $ans")
+  }
 
-  // Get the state and the result:
-  val (state1, result1) = a.run(10).value
-  // state: Int = 10
-  // result: String = The state is 10
-  println(s"${(state1, result1)}")
+  val both = for {
+    a <- step1
+    b <- step2
+  } yield (a, b)
 
-  // Get the state, ignore the result:
-  val state2 = a.runS(10).value
-  // state: Int = 10
-  println(s"$state2")
+  val (state, result) = both.run(20).value
+  println(s"${(state, result)}")
 
-  // Get the result, ignore the state:
-  val result2 = a.runA(10).value
-  // result: String = The state is 10
-  println(s"$result2")
+  println("##################")
 
+  val getDemo = State.get[Int] // State[Int,Int]
+  val rGet = getDemo.run(10).value
+  println(rGet)
+
+  val setDemo = State.set[Int](30) //State[Int, Unit]
+  val rSet = setDemo.run(10).value
+  println(rSet)
+
+  val pureDemo = State.pure[Int, String]("Result") //State[Int, String]
+  val rPure = pureDemo.run(10).value
+  println(rPure)
+
+  val inspectDemo = State.inspect[Int, String](_ + "!") //State[Int, String]
+  val rInspect = inspectDemo.run(10).value
+  println(rInspect)
+
+  val modifyDemo = State.modify[Int](_ + 1) //State[Int, Unit]
+  val rModify = modifyDemo.run(10).value
+  println(rModify)
+
+
+  println("#####################")
+
+
+  val program: State[Int, (Int, Int, Int)] = for {
+    a <- get[Int]
+    _ <- set[Int](a + 1)
+    b <- get[Int]
+    _ <- modify[Int](_ + 1)
+    c <- inspect[Int, Int](_ * 1000)
+  } yield (a, b, c)
+
+  val (stateProgram, resultProgram ) = program.run(1).value
+  println(s"${(stateProgram, resultProgram )}")
 }
