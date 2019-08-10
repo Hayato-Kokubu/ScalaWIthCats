2a80877284b1593648c5c00a861edf6224433d19
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-08-10 17:40:46 +0900

    end invariant

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,50 +1,29 @@
-import cats.{Contravariant, Show}
-import cats.instances.string._
-import cats.syntax.contravariant._
+import cats.Monoid
+// Symbol 版のMonoid を作る
+// Symbol はCats標準のインスタンスは用意していないので
+// 一度Symbol をStringで変換し、Monoid[String]で結合して Symbol で戻す
 
-import scala.language.higherKinds
+import cats.instances.string._
+import cats.syntax.invariant._
+import cats.syntax.monoid._
 
 import Implicits._
 
-
 object Main extends App {
 
-  val dC = implicitly[Codec[Double]]
 
-  println( dC.encode(123.4) )
+  println( Monoid[Symbol].empty )
+  println( 'a |+| 'few |+| 'words )
 
-}
-
-trait Codec[A] { self =>
-  def encode(value: A): String
-  def decode(value: String): A
-
-  def imap[B](dec: A => B, enc: B => A): Codec[B] =
-    new Codec[B] {
-      def encode(value: B): String = self.encode(enc(value))
-      def decode(value: String): B = dec(self.decode(value))
-    }
 }
 
 
 object Implicits {
-  implicit val stringCodec: Codec[String] = {
-    new Codec[String]{
-      def encode(value: String): String = value
-      def decode(value: String): String = value
-    }
-  }
-
-  implicit val intCodec: Codec[Int] = stringCodec.imap[Int](_.toInt, _.toString)
-  implicit val booleanCodec: Codec[Boolean] = stringCodec.imap[Boolean](_.toBoolean, _.toString)
-
-  implicit val doubleCodec: Codec[Double] = stringCodec.imap[Double](_.toDouble, _.toString)
-  implicit def boxCodec[A](implicit  codecA: Codec[A]): Codec[Box[A]] =
-//    stringCodec.imap[Box[A]](value => Box(codecA.decode(value)), _.toString)
-  codecA.imap(Box(_), _.value)
-
+  implicit val symbolMonoid: Monoid[Symbol] =
+//    new Monoid[Symbol] {
+//      def empty: Symbol = Symbol("")
+//      def combine(a: Symbol, b: Symbol) = Symbol(a.toString + b.toString)
+//  }
+  Monoid[String].imap(Symbol.apply)(_.name)
 
 }
-
-
-case class Box[A](value: A)
