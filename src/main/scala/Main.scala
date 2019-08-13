0160a4c0756da0b9102119e285d065a95697190d
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-08-13 23:22:01 +0900

    use imapN

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,26 +1,32 @@
+import cats.Monoid
+
+import cats.instances.string._
+import cats.instances.int._
+import cats.instances.list._
+
 import cats.syntax.apply._
-import cats.instances.option._
-
-
-import cats.syntax.option._
+import cats.instances.invariant._
 
 
 object Main extends App {
-  val a = (123.some, "abc".some).tupled // use Semigroupal[Option] from cats.syntax.option._
-  println(a)
 
-  val garfield =
-    (
-    Option("Garfield"),
-    Option(1978),
-    Option("Orange & black")
-  ).mapN(Cats)
+  val tupleToCat: (String, Int, List[String]) => Cat = Cat.apply _
 
+  val catToTuple: Cat => (String, Int, List[String]) = { cat =>
+    (cat.name, cat.yearOfBirth, cat.favoriteFoods)
+  }
 
-  println(garfield)
-
+  implicit val catMonid: Monoid[Cat] = (
+    Monoid[String],
+    Monoid[Int],
+    Monoid[List[String]]
+  ).imapN(tupleToCat)(catToTuple)
 
 }
 
 
-case class Cats(name: String, born: Int, color: String)
+case class Cat(
+                name: String,
+                yearOfBirth: Int,
+                favoriteFoods: List[String]
+              )
