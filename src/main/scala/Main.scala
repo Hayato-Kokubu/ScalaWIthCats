e3ff821709fe8275e1b62612a4951f35930e7eed
hayatokokubu <kokubu.hayato@gmail.com>
2019-08-15 13:07:35 +0900

    use Apply method for List... looks strange

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,38 +1,8 @@
 import cats.Semigroupal
-import cats.instances.future._
-import cats.syntax.apply._ // for mapN
-
-import scala.concurrent.{Await, Future}
-import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits.global
+import cats.instances.list._
 
 object Main extends App {
 
-  val futurePair = Semigroupal[Future].
-    product(Future("Hello"), Future(123))
-
-  println(futurePair)
-
-  Await.result(futurePair, 1.second)
-  println(futurePair)
-
-
-  val futureCat = (
-    Future{println("futureCat Name"); Thread.sleep(3000) ; "Garfield"},//("Garfield"),
-    Future{println("futureCat birth"); Thread.sleep(2000) ; 1978},//(1978),
-    Future{println("futureCat food "); Thread.sleep(1000) ; List("Lasagne")}//(List("Lasagne"))
-  ).mapN(Cat.apply)
-
-  println(futureCat)
-
-  Await.result(futureCat, 3.second)
-  println(futureCat)
-
-
+  val res = Semigroupal[List].product(List(1,2), List(3,4))
+  println(res)
 }
-
-case class Cat(
-  name: String,
-  yearOfBirth: Int,
-  favoriteFoods: List[String]
-)
