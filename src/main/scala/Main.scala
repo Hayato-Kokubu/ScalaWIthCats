f45ccc390455dbcc01779f8b7f945d5a3d34eff4
hayatokokubu <kokubu.hayato@gmail.com>
2019-08-16 11:52:18 +0900

    use Validatino smart constructor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,28 +1,13 @@
-import cats.Semigroupal
 import cats.data.Validated
-import cats.instances.list._ // ないとエラーは検知してくれないが、Validated の色が変わる
-import cats.instances.either._
+
 
 object Main extends App {
+  val v = Validated.Valid(123)
+  val i = Validated.Invalid(List("Badness"))
 
-  type AllErrorsOr[A] = Validated[List[String], A]
+  println(v)
+  println(i)
 
-  val a =
-    Semigroupal[AllErrorsOr].product(
-      Validated.invalid(List("Error 1")),
-      Validated.invalid(List("Error 2"))
-  )
 
-  println(a)
-
-  type ErrorsOr[A] = Either[List[String], A]
-
-  val b =
-    Semigroupal[ErrorsOr].product(
-      Left(List("Error 1")),
-      Left(List("Error 2"))
-    )
-
-  println(b)
 
 }
