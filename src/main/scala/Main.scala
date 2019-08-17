ddf6f85ba20d6b287e5c854237c7e2afc28912cc
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-08-17 12:06:30 +0900

    Methods of Validated

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,39 +1,16 @@
-import cats.Semigroupal
-import cats.data.{NonEmptyVector, Validated}
-import cats.syntax.apply._
 import cats.syntax.validated._
-import cats.instances.string._
-import cats.instances.vector._
 
 object Main extends App {
+  val a = 123.valid.map(_ * 100)
+  val b = "?".invalid.leftMap(_.toString) // toString 意味ある？
+  println(b)
+  val b_ = "?".invalid
+    println(b_)
 
-  type AllErrorsOr[A] = Validated[String, A]
 
-  Semigroupal[AllErrorsOr]
-
-  val t =
-    (
-      12.valid[String],
-      //"Error 1".invalid[Int],
-      "Error 2".invalid[Int]
-    ).tupled // tupled がIntelliJで認識してもらえないが、動く。。。
-
-  println(t)
-
-  val t2 =
-    (
-      Vector(404).invalid[Int],
-      Vector(500).invalid[Int]
-    ).tupled
-
-  println(t2)
-
-  val t3 =
-    (
-      NonEmptyVector.of("Error 1").invalid[Int],
-      NonEmptyVector.of("Error 2").invalid[Int]
-    ).tupled
-
-  println(t3)
+  val c = 123.valid[String].ensure("Negative")(_ > 0)
+  println(c)
+  val c1 = (-123).valid[String].ensure("Negative")(_ > 0)
+  println(c1)
 
 }
