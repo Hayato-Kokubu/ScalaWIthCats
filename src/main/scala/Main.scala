a9a8db016a5829f3024430e0a6c40eb6392a8b2d
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-08-17 11:30:33 +0900

    helper methods to create instances of Validated from different sources

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,29 @@
 import cats.data.Validated
-
-import cats.syntax.applicative._
-import cats.syntax.applicativeError._
-
-import cats.instances.list._
+import cats.syntax.option._
 
 object Main extends App {
-  type ErrorsOr[A] = Validated[List[String], A]
+  val a = Validated.catchOnly[NumberFormatException]("foo".toInt)
+  println(a)
 
-  val v = 123.pure[ErrorsOr]
-  val i = List("Badness").raiseError[ErrorsOr, Int]
+  val a1 = Validated.catchOnly[NumberFormatException]("123".toInt)
+  println(a1)
+
+
+//  val a2 = Validated.catchOnly[NullPointerException]("foo".toInt)
+//  println(a2)
+
+  // sys.error creates new RuntimeException
+  val b = Validated.catchNonFatal(sys.error("Badness"))
+  println(b)
+
+  val c = Validated.fromEither[String, Int](Left("Badness"))
+  println(c)
+  val c1 = Validated.fromEither[String, Int](Right(1234))
+  println(c1)
+
+  val d = Validated.fromOption[String, Int](None, "Badness")
+  println(d)
+  val d1 = Validated.fromOption[String, Int](12345.some, "Badness")
+  println(d1)
 
 }
