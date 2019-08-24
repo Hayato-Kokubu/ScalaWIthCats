b58611eadda6ecd35118e1a636dd6be9baea1dbf
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-08-24 16:52:43 +0900

    traverse more absolutely

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,14 @@
-import scala.concurrent._
+import scala.language.higherKinds
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
-
-
-import cats.syntax.applicative._
+import cats.Applicative
 import cats.instances.future._
-import cats.syntax.apply._ // use mapN
+import cats.syntax.applicative._
+import cats.syntax.apply._
+
+
+
 
 object Main extends App {
 
@@ -18,23 +21,20 @@ object Main extends App {
   def getUptime(hostname: String): Future[Int] =
     Future(hostname.length * 60) // just for demonstration
 
+  val totalUptime = listTraverse(hostnames)(getUptime)
 
-  List.empty[Int].pure[Future]
+  // 3 seconds だと よくわからないエラーとなる。。。
+  Await.result(totalUptime, 3.seconds)
+  println(totalUptime)
 
-  def oldCombine(
-    accum : Future[List[Int]],
-    host  : String
-  ): Future[List[Int]] = {
-    val uptime = getUptime(host)
-    for {
-      accum  <- accum
-      uptime <- uptime
-    } yield accum :+ uptime
-  }
 
-  def newCombine(
-    accum: Future[List[Int]],
-    host: String
-  ): Future[List[Int]] =
-    (accum, getUptime(host)).mapN(_ :+ _)
+  def listTraverse[F[_]: Applicative, A, B]
+  (list: List[A])(func: A => F[B]): F[List[B]] =
+    list.foldLeft(List.empty[B].pure[F]) { (accum, item) =>
+      (accum, func(item)).mapN(_ :+ _)
+    }
+
+  def listSequence[F[_]: Applicative, B]
+  (list: List[F[B]]): F[List[B]] =
+    listTraverse(list)(identity)
 }
