9db174e95868c2bcf34a092fbfb638b59fe17ee6
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-09-15 17:58:10 +0900

    wip

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-import exercise.{A1, A2, MonoidTester, MyTarget}
+import exercise._
 
 object Main extends App{
   val t = System.currentTimeMillis
@@ -9,38 +9,51 @@ object Main extends App{
       // 現状、n^2個書かないといけないのは辛い。
       a11 <- MyTarget.values
       a12 <- MyTarget.values
+      a13 <- MyTarget.values
       a21 <- MyTarget.values
       a22 <- MyTarget.values
+      a23 <- MyTarget.values
+      a31 <- MyTarget.values
+      a32 <- MyTarget.values
+      a33 <- MyTarget.values
     } yield { (a1: MyTarget, a2: MyTarget) =>
       (a1, a2) match {
         case (A1, A1) => a11
         case (A1, A2) => a12
+        case (A1, E)  => a13
         case (A2, A1) => a21
         case (A2, A2) => a22
+        case (A2, E)  => a23
+        case (E, A1)  => a31
+        case (E, A2)  => a32
+        case (E, E)   => a33
         case _ => throw new IllegalArgumentException(s"invalid Target: ${(a1, a2)}")
       }
     }
 
-  // n
-  val identityCandidates = Seq(A1, A2)
-
   val targetMonoids = for {
     op <- allTargetOperations
-    e <- identityCandidates
-    if new MonoidTester(e)(op).test
-  } yield (op, e)
+    if new MonoidTester(op).test
+  } yield op
 
-  targetMonoids.foreach{case(op, e) =>
-    (
-      for {
-        x <- MyTarget.values
-        y <- MyTarget.values
-      }yield s"${(x, y)} -> ${op(x,y)}, "
-    ).foreach(print)
-    println()
-    println(s"enpty = $e")
-  }
+  targetMonoids.foreach{case op => printMonoid(op)}
   println(s"${System.currentTimeMillis - t} msc")
+
+  def printMonoid(op: (MyTarget, MyTarget) => MyTarget): Unit = {
+    for{
+     x <- MyTarget.values
+     y <- MyTarget.values
+    }yield{
+      println("### start ###")
+      println(s"  | A1 A2 E")
+      println(s"--|------------")
+      println(s"A1| ${op(A1, A1)} ${op(A1, A2)} ${op(A1, E)}")
+      println(s"A2| ${op(A2, A1)} ${op(A2, A2)} ${op(A2, E)}")
+      println(s"E | ${op(E, A1)} ${op(E, A2)} ${op(E, E)}")
+      println("###  end  ###")
+    }
+
+  }
 
 }
 
--- src/main/scala/exercise/MonoidTester.scala
+++ src/main/scala/exercise/MonoidTester.scala
@@ -1,7 +1,7 @@
 package exercise
 
 
-class MonoidTester(e: MyTarget)( * : (MyTarget, MyTarget) => MyTarget ) {
+class MonoidTester( * : (MyTarget, MyTarget) => MyTarget ) {
 
   def assosiativeLow: Boolean =
     (
@@ -14,17 +14,18 @@ class MonoidTester(e: MyTarget)( * : (MyTarget, MyTarget) => MyTarget ) {
 
 
   def identityLow: Boolean =
-    MyTarget.values.forall{a => *(a , e) == a && *(e , a) == a}
+    MyTarget.values.forall{a => *(a , E) == a && *(E , a) == a}
 
   def test = assosiativeLow && identityLow
 }
 
 sealed trait MyTarget {
 }
+case object E  extends MyTarget
 case object A1 extends MyTarget
 case object A2 extends MyTarget
 
 object MyTarget{
   // いちいち定義しないでやりたい。。。
-  val values: Seq[MyTarget] = Seq(A1, A2)
+  val values: Seq[MyTarget] = Seq(E, A1, A2)
 }
